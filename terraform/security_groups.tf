ece755ae547961da5451ad2b51a8df5d73da43b7
satish <pathadesatish0@gmail.com>
2026-03-05 13:32:59 +0530

    update security group file

--- terraform/security_groups.tf
+++ terraform/security_groups.tf
@@ -60,7 +60,7 @@ resource "aws_security_group" "ec2" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = var.allowed_ssh_cidr
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
